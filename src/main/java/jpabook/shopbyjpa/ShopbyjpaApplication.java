bfe86e80ac6e71f88d0b1f750f1484e0ddb35773
Kim munsoo <kms7562127@gmail.com>
2022-12-14 16:48:18 +0900

    스프링 활용2 - API 개발 # 회원조회 @GetMapping("api/v1/members") @GetMapping("api/v2/members")
    # 회원가입
    @PostMapping("/api/v1/members")
    @PostMapping("/api/v2/members")
    
    # 회원정보 수정(예제기 때문에 이름만 수정)
    @PutMapping("/api/v2/members/{id}")
    
    # 주문조회
    @GetMapping("api/v1/simple-orders")
    @GetMapping("api/v2/simple-orders")
    @GetMapping("api/v3/simple-orders")
    @GetMapping("api/v4/simple-orders")
    v number가 높을수록 최적화 등 성능적으로 높은 효과를 발휘할 수 있음

--- src/main/java/jpabook/shopbyjpa/ShopbyjpaApplication.java
+++ src/main/java/jpabook/shopbyjpa/ShopbyjpaApplication.java
@@ -1,7 +1,9 @@
 package jpabook.shopbyjpa;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShopbyjpaApplication {
@@ -9,4 +11,8 @@ public class ShopbyjpaApplication {
 		SpringApplication.run(ShopbyjpaApplication.class, args);
 	}
 
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
+	}
 }
